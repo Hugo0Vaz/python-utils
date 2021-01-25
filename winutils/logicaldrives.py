abf8f328fb893ec96853c5eacff3bb7c946ced05
Hugo Vaz <hugomartinsvaz@gmail.com>
2021-01-25 14:09:59 -0300

    Primeiro commit, criação do logicaldrives

---
+++ winutils/logicaldrives.py
@@ -0,0 +1,19 @@
+#==============================================================================#
+#                                                                              #
+#                              Logical Drives                                  #
+# Descrição: Script que retorna os dispositivos lógicos presentes na máquina   #
+# Autores: Hugo Martins Vaz Silva - hugomartinsvaz@gmail.com                   #
+#                                   hugo.martins@kot.com.br                    #
+#==============================================================================#
+
+import winreg
+
+def get_logical_drives():
+    drives = []
+    bitmask = windll.kernel32.GetLogicalDrives()
+    for letter in string.ascii_uppercase:
+        if bitmask & 1:
+            drives.append(letter+':\\')
+        bitmask >>= 1
+
+    return drives
