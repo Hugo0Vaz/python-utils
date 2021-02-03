cf06f4c855ada6331c7456475a38e06e595e09db
Hugo Vaz <hugomartinsvaz@gmail.com>
2021-02-03 12:56:31 -0300

    Colocando os imports __init__.py

---
+++ winutils/__init__.py
@@ -0,0 +1,10 @@
+
+from .installedapps import get_win_32_apps
+from .installedapps import get_win_64_apps
+from .installedapps import get_win_std_apps
+from .installedapps import get_all_apps
+from .installedapps import get_regs
+
+from .logicaldrives import get_logical_drives
+
+__version__ = '0.0.2'
